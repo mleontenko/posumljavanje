ab0ee728c11be9db5c93bd2f9c3da14ccf097057
mleontenko <marin.leontenko@gmail.com>
2019-09-30 14:40:28 +0200

    modified routes & layouts

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/map';
 
     /**
      * Create a new controller instance.
